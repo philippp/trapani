dad53a793809d218dc4a2bb2629999f0334eae25
Philippp <philippp@gmail.com>
2020-03-09 23:13:49 -0700

    Added most of the install steps and dev key upload

--- gcloud/setup.sh
+++ gcloud/setup.sh
@@ -1,1 +1,13 @@
-pip install --upgrade google-cloud-spanner google-cloud-pubsub twilio
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 install --upgrade google-cloud-spanner google-cloud-pubsub twilio python-dateutil
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh
+sudo apt-get update
+sudo apt-get install stackdriver-agent
+sudo service stackdriver-agent start
+echo "export GOOGLE_APPLICATION_CREDENTIALS=\"/home/philippp/keys/Trapani-9ff766720b9f.json\"" >> ~/.bashrc
+# systemd requires a temp directory that's usually set by X, but missing on headless clients.
+echo "export XDG_RUNTIME_DIR=/tmp" >> ~/.bashrc
+source ~/.bashrc
--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 tar -cvzf bundle.tgz `git ls-tree -r master --name-only`
 gcloud compute scp bundle.tgz dispatcher-1:~/ --zone=us-central1-a
+gcloud compute scp --recurse keys dispatcher-1:~/
